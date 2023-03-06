92a6ac1e30175baaa782bd08f5244a005111a84b
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-06 11:23:02 +0100

    spring toegevoegd

---
+++ swdn/src/main/java/com/itvitae/swdn/SwdnApplication.java
@@ -0,0 +1,13 @@
+package com.itvitae.swdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwdnApplication.class, args);
+	}
+
+}
---
+++ swdn/src/test/java/com/itvitae/swdn/SwdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvitae.swdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
