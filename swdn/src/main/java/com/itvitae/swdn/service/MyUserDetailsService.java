a6e2b518c3dc4dc45d82f32723e11a3ad3573f2f
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-08 13:15:13 +0100

    Added basic security

---
+++ swdn/src/main/java/com/itvitae/swdn/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.itvitae.swdn.service;
+
+import com.itvitae.swdn.model.MyUserDetails;
+import com.itvitae.swdn.model.User;
+import com.itvitae.swdn.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+
+        return new MyUserDetails(user.get());
+    }
+}
