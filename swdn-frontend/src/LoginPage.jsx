013e6d111e20a1509c55d6de1c0500cf0fda6386
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-10 15:52:28 +0100

    Added skeleton of login page

---
+++ swdn-frontend/src/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LoginPage(props) {
+    return (
+        <div>
+            <div className="title">
+                <h1>XXXX Coaching App</h1>
+            </div>
+        </div>
+    );
+}
