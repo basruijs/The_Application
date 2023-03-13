e3e563643c7f1bfa5c3866897df7ee3b61a9c289
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-13 10:33:50 +0100

    login backend

--- swdn/src/main/java/com/itvitae/swdn/configuration/SecurityConfig.java
+++ swdn/src/main/java/com/itvitae/swdn/configuration/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.itvitae.swdn.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,5 +42,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
