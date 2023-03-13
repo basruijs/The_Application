14ef1df9134f365982342b0dbc775b80bcd1e3c2
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-13 10:40:48 +0100

    Login frontend

--- swdn-frontend/src/LoginPage.jsx
+++ swdn-frontend/src/LoginPage.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LoginPage(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function checkCredentials() {
+        const loginCredentials = JSON.stringify({
+            email: email,
+            password: password,
+        });
+
+        fetch(`http://localhost:8082/api/user/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: loginCredentials,
+        }).then(
+            () => {
+                props.setEmail(email);
+                props.setPassword(password);
+            },
+            () => {
+                alert('Incorrect username or password.');
+            }
+        );
+    }
+
     return (
         <div>
             <div className="title">
                 <h1>XXXX Coaching App</h1>
             </div>
+            <form
+                className="login-wrapper"
+                onSubmit={(e) => {
+                    checkCredentials();
+                    e.preventDefault();
+                }}
+            >
+                <h2>Login</h2>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="submit"
+                    value="Log in"
+                />
+            </form>
         </div>
     );
 }
