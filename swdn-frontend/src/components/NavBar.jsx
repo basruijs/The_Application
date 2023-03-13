0c70bc115e941ee9306b9a2a589c62df30a96b07
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-13 15:56:06 +0100

    Added styling

--- swdn-frontend/src/components/NavBar.jsx
+++ swdn-frontend/src/components/NavBar.jsx
@@ -6,14 +6,29 @@ function NavBar(props) {
         <div className="navBar bordered">
             <h2>{props.person.name || ''}</h2>
             <nav>
-                <NavLink to="/login">Login</NavLink>
+                <NavLink
+                    to="/login"
+                    className="navButton"
+                >
+                    Login
+                </NavLink>
                 {props.person.role.name === 'HR' ? (
-                    <NavLink to="/hr">Dashboard</NavLink>
+                    <NavLink
+                        to="/hr"
+                        className="navButton"
+                    >
+                        Dashboard
+                    </NavLink>
                 ) : (
                     ''
                 )}
                 {props.person.role.name === 'COACH' ? (
-                    <NavLink to="/coach">Dashboard</NavLink>
+                    <NavLink
+                        to="/coach"
+                        className="navButton"
+                    >
+                        Dashboard
+                    </NavLink>
                 ) : (
                     ''
                 )}
