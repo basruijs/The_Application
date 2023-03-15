42163df9ac7ab09474cd7e6a21c89c52aa0ac1cd
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-15 13:26:46 +0100

    Handle accepting/denying change requests

--- swdn/src/main/java/com/itvitae/swdn/repository/ChangeRequestRepository.java
+++ swdn/src/main/java/com/itvitae/swdn/repository/ChangeRequestRepository.java
@@ -1,9 +1,13 @@
 package com.itvitae.swdn.repository;
 
 import com.itvitae.swdn.model.ChangeRequest;
+import com.itvitae.swdn.model.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChangeRequestRepository extends CrudRepository<ChangeRequest, Long> {
+    Optional<ChangeRequest> findFirstByRequester(Person requester);
 }
--- swdn/src/main/java/com/itvitae/swdn/service/ChangeRequestService.java
+++ swdn/src/main/java/com/itvitae/swdn/service/ChangeRequestService.java
@@ -1,15 +1,17 @@
 package com.itvitae.swdn.service;
 
 import com.itvitae.swdn.dto.ChangeRequestDto;
+import com.itvitae.swdn.dto.ChangeRequestGetDto;
 import com.itvitae.swdn.mapper.ChangeRequestMapper;
 import com.itvitae.swdn.model.ChangeRequest;
 import com.itvitae.swdn.model.Person;
-import com.itvitae.swdn.model.Skill;
 import com.itvitae.swdn.repository.ChangeRequestRepository;
 import com.itvitae.swdn.repository.PersonRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,11 +25,29 @@ public class ChangeRequestService {
 
     @Autowired
     PersonRepository personRepository;
+
     public void addChangeRequest(ChangeRequestDto changeRequestDto, long personid) {
         ChangeRequest newChangeRequest = changeRequestMapper.toEntity(changeRequestDto);
         Person person = personRepository.findById(personid).get();
         person.setChangeRequest(newChangeRequest);
         newChangeRequest.setRequester(person);
         changeRequestRepository.save(newChangeRequest);
+    }
+
+    public ChangeRequestGetDto getRequestByPerson(long id) {
+        Optional<Person> foundPerson = personRepository.findById(id);
+        if (!foundPerson.isPresent()) {
+            throw new IllegalArgumentException("No such person");
+        }
+        Person person = foundPerson.get();
+        Optional<ChangeRequest> foundRequest = changeRequestRepository.findFirstByRequester(person);
+        return foundRequest.map(changeRequest -> changeRequestMapper.toDto(changeRequest)).orElse(new ChangeRequestGetDto());
+    }
+
+    public void deleteChangeRequest(long id) {
+        if (!changeRequestRepository.existsById(id)) {
+            throw new IllegalArgumentException("No such request");
+        }
+        changeRequestRepository.deleteById(id);
     }
 }
