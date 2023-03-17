dcc6791d62d17f5678a62fa773a2f09d77786110
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-17 10:55:47 +0100

    het werkt nog niet :sunglasses:
    maar ik doe even een commit voor als ik ontplof :sunglasses:

--- swdn-frontend/src/components/FeedbackEditor.jsx
+++ swdn-frontend/src/components/FeedbackEditor.jsx
@@ -1,5 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 export default function SkillEditor(props) {
-    return <div>sunglasses emoji (dit is nog niet af)</div>;
+    return (
+        <div className="feedbackOverview bordered">
+            <input
+                type="submit"
+                className="edit bordered"
+                value="✓"
+            />
+            <h2>{props.feedback.name}</h2>
+            <p>{props.feedback.sendDate}</p>
+            <div className="info-flex">
+                <form action="">
+                    <textarea className="feedbackInputBox"></textarea>
+                </form>
+            </div>
+        </div>
+    );
 }
