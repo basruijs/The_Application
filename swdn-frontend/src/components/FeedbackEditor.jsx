e2c9bb83cd3ffd0014deb69514900ca29f08c2c7
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-17 11:38:45 +0100

    Het werkt?

--- swdn-frontend/src/components/FeedbackEditor.jsx
+++ swdn-frontend/src/components/FeedbackEditor.jsx
@@ -1,20 +1,47 @@
 import React, { useEffect, useState } from 'react';
 
 export default function SkillEditor(props) {
+    const [feedback, setFeedback] = useState(props.feedback.feedback);
+
+    function sendChanges() {
+        const newFeedback = JSON.stringify({
+            feedback: feedback,
+        });
+        fetch(
+            `http://localhost:8082/api/invitation/update/${props.feedback.id}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization:
+                        'Basic ' + btoa(props.email + ':' + props.password),
+                },
+                body: newFeedback,
+            }
+        ).then(() => props.update());
+    }
     return (
         <div className="feedbackOverview bordered">
-            <input
-                type="submit"
-                className="edit bordered"
-                value="✓"
-            />
-            <h2>{props.feedback.name}</h2>
-            <p>{props.feedback.sendDate}</p>
-            <div className="info-flex">
-                <form action="">
-                    <textarea className="feedbackInputBox"></textarea>
-                </form>
-            </div>
+            <form
+                onSubmit={(e) => {
+                    sendChanges();
+                    props.setEdit(false);
+                    e.preventDefault();
+                }}
+            >
+                <input
+                    type="submit"
+                    className="edit bordered"
+                    value="✓"
+                />
+                <h2>{props.feedback.name}</h2>
+                <p>{props.feedback.sendDate}</p>
+                <textarea
+                    className="feedbackInputBox"
+                    value={feedback || ''}
+                    onChange={(e) => setFeedback(e.target.value)}
+                ></textarea>
+            </form>
         </div>
     );
 }
