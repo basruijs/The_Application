9c7e6abcfbb6f25aed6cb6ad02e9203becb285ee
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-20 15:17:59 +0100

    Log out button
    for the purpose of escaping the german mafia

--- swdn-frontend/src/components/NavBar.jsx
+++ swdn-frontend/src/components/NavBar.jsx
@@ -14,12 +14,20 @@ function NavBar(props) {
                         Login
                     </NavLink>
                 ) : (
-                    <NavLink
-                        to="/account"
-                        className="navButton"
-                    >
-                        Account
-                    </NavLink>
+                    <div>
+                        <NavLink
+                            to="/account"
+                            className="navButton"
+                        >
+                            Account
+                        </NavLink>
+                        <a
+                            href="/login"
+                            className="logout"
+                        >
+                            logout
+                        </a>
+                    </div>
                 )}
 
                 {props.person.role.name === 'HR' ? (
