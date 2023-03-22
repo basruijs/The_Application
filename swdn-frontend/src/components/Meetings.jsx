95e4f36080b906b23faee5d02519565617137b62
Bas Ruijs <62066420+basruijs@users.noreply.github.com>
2023-03-22 11:38:29 +0100

    Je kan niet meer 2 dezelfde emails gebruiken voor users
    Als je dat wel doet, gaan allebei de accounts kapot en kan je niet meer inloggen

--- swdn-frontend/src/components/Meetings.jsx
+++ swdn-frontend/src/components/Meetings.jsx
@@ -3,22 +3,6 @@ import Meeting from './Meeting';
 
 function Meetings(props) {
     function sortMeetings() {
-        // const currentDate = new Date();
-        // let date = currentDate.getFullYear() + '-';
-        // if (currentDate.getMonth() < 10) {
-        //     date += '0';
-        // }
-        // date += currentDate.getMonth() + 1 + '-' + currentDate.getDate();
-        // let time = '';
-        // if (currentDate.getHours() < 10) {
-        //     time += '0';
-        // }
-        // time += currentDate.getHours() + ':';
-        // if (currentDate.getMinutes() < 10) {
-        //     time += '0';
-        // }
-        // time += currentDate.getMinutes();
-
         const meetings = props.meetings;
 
         meetings.sort((a, b) =>
@@ -27,15 +11,6 @@ function Meetings(props) {
         meetings.sort((a, b) =>
             a.date > b.date ? 1 : b.date > a.date ? -1 : 0
         );
-
-        // for (let index = 0; index < meetings.length; index++) {
-        //     const meeting = meetings[index];
-        //     if (date >= meeting.date) {
-        //         if (time >= meeting.time) {
-        //             meetings.splice(index, 1);
-        //         }
-        //     }
-        // }
 
         return meetings;
     }
