8addabc167d61a58092fc1beecb4587bb27571ac
MennoMuller <104193604+MennoMuller@users.noreply.github.com>
2023-03-22 12:04:45 +0100

    Added ChangeRequestApi

---
+++ swdn/src/main/java/com/itvitae/swdn/controller/ChangeRequestApi.java
@@ -0,0 +1,50 @@
+package com.itvitae.swdn.controller;
+
+import com.itvitae.swdn.dto.ChangeRequestDto;
+import com.itvitae.swdn.dto.ChangeRequestGetDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "changerequest", description = "change request api")
+@RequestMapping("/api/changerequest")
+public interface ChangeRequestApi {
+    @Operation(
+            summary = "Creates a new change request",
+            description = "Creates a new change request for the specified person",
+            tags = {"changerequest"}
+    )
+    @ApiResponse(responseCode = "200", description = "Successful operation")
+    @PostMapping("/new/{personid}")
+    void newChangeRequest(@RequestBody ChangeRequestDto changeRequestDto, @PathVariable(value = "personid") long personid);
+
+    @Operation(
+            summary = "Returns a change request",
+            description = "Returns the open change request for the specified person",
+            tags = {"changerequest"}
+    )
+    @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(implementation = ChangeRequestGetDto.class)))
+    @GetMapping(value = "/byperson/{id}", produces = {"application/json"})
+    ChangeRequestGetDto getRequestByPerson(@PathVariable(value = "id") long id);
+
+    @Operation(
+            summary = "Deletes a change request",
+            description = "Deletes the change request with the specified id",
+            tags = {"changerequest"}
+    )
+    @ApiResponse(responseCode = "200", description = "Successful operation")
+    @DeleteMapping("/delete/{id}")
+    void deleteChangeRequest(@PathVariable(value = "id") long id);
+
+    @Operation(
+            summary = "Denies a change request",
+            description = "Denies the change request with the specified id, for the reason specified in the body.",
+            tags = {"changerequest"}
+    )
+    @ApiResponse(responseCode = "200", description = "Successful operation")
+    @DeleteMapping("/deny/{id}")
+    void denyChangeRequest(@PathVariable(value = "id") long id, @RequestBody String message);
+}
